729e94ab1efd544c66ef9c9e3dc0430787af58b6
rurz <arurz@inaoep.mx>
2020-12-30 23:16:38 -0600

    Optimized `kravmat` function with preallocation

--- src/PixelsGT.jl
+++ src/PixelsGT.jl
@@ -21,12 +21,22 @@ end
 A(n, j) = ((-1)^n / 2^j)
 B(n, j) = binomial(2 * j, n)
 
-kravfunc(n,q,j) = A(n, j) * sqrt(B(n, j) * B(j + q, j)) * Kpoly(n, j + q, j)
+kravfunc(n,q,j) = A(n, j) * sqrt(B(n, j) * B(j + q, j)) * kravpol(n, j + q, j)
 
 # Construct the Kravchuk matrix of (N+1) x (N+1) dimensions who columns are
 # energy levels n, and rows are position index q
 
-kravmat(j::Integer) = reshape(
-                        [Kfunc(l, m, j) for m in -j:j
-                                            for l in 0:2 * j],
-                                                (2 * j + 1, 2 * j + 1))
+# kravmat(j::Integer) = reshape(
+#                        [kravfunc(l, m, j) for m in -j:j
+#                                            for l in 0:2 * j],
+#                                                (2 * j + 1, 2 * j + 1))
+
+function kravmat(j::Integer)
+	kvec = zeros(Float64, (2 * j + 1, 2 * j + 1))
+	for l in -j:j
+		for m in 0:2 * j
+			kvec[m + 1, l + 1 + j] = kravfunc(m, l, j)
+		end
+	end
+	return kvec
+end
