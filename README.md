fe468ec2c2569abe9a717f781e40dcdc696a0dd9
arurz <arurz@inaoep.mx>
2021-05-24 20:27:27 -0500

    Updated README and Create Branch
    * README v0.2
    * Created branch for optimization tryings

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # PixelsGT.jl - A package to unitarily transform pixellated screens
+
+### Goal of the package
+
+Provide a set of methods to evidence the powerfulness of the unitary transformations arising from the symmetries of the compact Lie groups
+
+### Caveats
+
+Just one: **It is incredibily slow!** — Because of the complexity of the algorithm (~ O(N⁴)), this method to compute transformations of 2D images are, unfortunately, the most resources expensive, and truly slow you can find. _This challenge isn't for the impatient_
+
+___
+### Usage
+
+At this time (v.0.1.0), there is all set to take a PNG image of N×N pixels, and make a (counterclockwise) rotation given a rotation angle θ.
+
+`Fqθ(θ, q₁, q₂, j, imgdat)` where j is the size of the representation, thus, N = 2j + 1; θ is the rotation angle; (q₁, q₂) ∈ {-j, j} are the indexes of the two-dimensional array; and, imgdat is the two-dimensional array containing the values of the pixels.
+
+```
+julia> Fqθ(θ, q₁. q₂, j, imgdat)
+
+julia> "Value of the imgdat pixel (q₁, q₂) when rotated θ"
+```
+
+___
+Kindly cheers, from the developer, Alejandro R. Urzúa. 😺
--- src/LKrav.jl
+++ src/LKrav.jl
@@ -4,7 +4,7 @@ export  ϕd, ϕu, LKd, LKu, LK
 
 "`Φd(q₁, q₂, n, m, j)` shows the rotations of the Kravchuk function modes at (n, |m| ≤ n) for n ∈ [0, 2j]. It needs to be evaluted at (q₁, q₂) ∈ [0, 2j + 1]."
 function Φd(q₁, q₂, n, m, j)
-    v₀ = zeros(ComplexF64, 2 * j + 1)
+    v₀ = zeros(2 * j + 1)
     for n₁ in 0:n
         v₀[n₁ + 1] = Φ2(n₁, n - n₁, q₁, q₂, j) * dd(n, j)[n₁ + 1, Integer((m + n)/2) + 1]
     end
@@ -13,7 +13,7 @@ end
 
 "`Φu(q₁, q₂, n, m, j)` shows the rotations of the Kravchuk function modes at (n, |m| ≤ 4j - n) for n ∈ [4j, 2j]. It needs to be evaluted at (q₁, q₂) ∈ [0, 2j + 1]."
 function Φu(q₁, q₂, n, m, j)
-    v₁ = zeros(ComplexF64, 2 * j + 1)
+    v₁ = zeros(2 * j + 1)
     for n₁ in 0:(4* j - n)
         v₁[n₁ + 1] = Φ2(2 * j - n₁, n + n₁ - 2 * j, q₁, q₂, j) * du(n, j)[n₁ + 1, Integer((m + 4 * j - n)/2) + 1]
     end
@@ -28,16 +28,16 @@ function LKd(q₁, q₂, n, m, j)
     for n₁ in 0:n
         vlkd[n₁ + 1] = (-1.0im)^(n - n₁) * Φ2(n₁, n - n₁, q₁, q₂, j) * dd(n, j)[n₁ + 1, Integer((m + n)/2) + 1]
     end
-    return (-1.0)^((1/2) * (abs(m) - m)) * sum(vlkd)
+    return (-1.0)^((0.5) * (abs(m) - m)) * sum(vlkd)
 end
 
 "`LKu(q₁, q₂, n, m, j)` shows the Laguerre-Kravchuk modes at (n, |m| ≤ 4j - n) for n ∈ [4j, 2j]. It needs to be evaluted at (q₁, q₂) ∈ [0, 2j + 1]."
 function LKu(q₁, q₂, n, m, j)
-    vlku= zeros(ComplexF64, 2 * j + 1)
+    vlku = zeros(ComplexF64, 2 * j + 1)
     for n₁ in 0:(4* j - n)
         vlku[n₁ + 1] = (-1.0im)^(n - n₁) * Φ2(2 * j - n₁, n + n₁ - 2 * j, q₁, q₂, j) * du(n, j)[n₁ + 1, Integer((m + 4 * j - n)/2) + 1]
     end
-    return (-1.0)^((1/2) * (abs(m) - m)) * sum(vlku)
+    return (-1.0)^((0.5) * (abs(m) - m)) * sum(vlku)
 end
 
 "`LK(q₁, q₂, n, m, j)` gives the complete Laguerre-Kravchuk modes. Their index ranges are n ∈ [0, 4j] and m ∈ [-n,n] in steps of 2."
